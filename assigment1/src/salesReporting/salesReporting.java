2c3eef5e0336dfbd34a0ca5983cac2a458dc1e1d
Dante-Ali <77851236+Dante-Ali@users.noreply.github.com>
2021-02-22 13:58:11 +0100

    Pushing salesReporting for testing

---
+++ assigment1/src/salesReporting/salesReporting.java
@@ -0,0 +1,30 @@
+package salesReporting;
+
+public class salesReporting {
+
+    String region;
+    String country;
+    String itemType;
+    String salesChannel;
+    int orderPriority;
+    int orderDate;
+
+    /*Order ID,
+
+    Ship Date,
+
+“Finanser” {
+
+        Units Sold,
+
+        Unit Price,
+
+        Unit Cost,
+
+        Total Revenue,
+
+        Total Cost,
+
+        Total Profit}*/
+
+}
