9ac45d5ceb0316227af698c423c9da71f3595125
Petter Tondel <petter.toendel@outlook.com>
2021-02-23 12:51:38 +0100

    Fikset try/Catch i OpenFile.java. Mer funksjonalitet kommer

--- assignment1/src/assignment/OpenFile.java
+++ assignment1/src/assignment/OpenFile.java
@@ -1,17 +1,16 @@
 package assignment;
 
 public class OpenFile {
-/*
-        try {
-            ReadClass readClass = new ReadClass();
-            readClass.setFilnavn("assignment1/SalesRecords3.csv");
-            readClass.readEverything();
-            readClass.printEverythingInObjects();
-            readClass.readOnlySpecificColumns();
-        } catch (NullPointerException exception) {
-            System.out.println("Not able to open file - wrong filename");
-        }
-    }
-*/
 
+        public void read() {
+            try {
+                ReadClass readClass = new ReadClass();
+                readClass.setFilnavn("assignment1/SalesRecords3.csv");
+                readClass.readEverything();
+                readClass.printEverythingInObjects();
+                readClass.readOnlySpecificColumns();
+            } catch (NullPointerException exception) {
+                System.out.println("Not able to open file - wrong filename");
+            }
+        }
 }
