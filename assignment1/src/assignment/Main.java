978fd2d20ddf04e8914bab8179a83b7508d177f3
Knut Maurset <knut.espen.maurset@dnb.no>
2021-02-23 11:16:39 +0100

    Første meny oppsett

--- assignment1/src/assignment/Main.java
+++ assignment1/src/assignment/Main.java
@@ -1,10 +1,27 @@
 package assignment;
 
+import assignment.dependencies.Ui_fromScanner;
+
 public class Main {
+
+
     public static void main(String[] args) {
-        ReadClass readClass = new ReadClass();
-        readClass.setFilnavn("assignment1/SalesRecords2.csv");
-        readClass.readEverything();
-        readClass.printEverythingInObjects();
+
+
+        Menu instanciateMenu = new Menu();
+        instanciateMenu.menu(new Ui_fromScanner());
     }
+    /*
+        try {
+            ReadClass readClass = new ReadClass();
+            readClass.setFilnavn("assignment1/SalesRecords3.csv");
+            readClass.readEverything();
+            readClass.printEverythingInObjects();
+            readClass.readOnlySpecificColumns();
+        } catch (NullPointerException exception) {
+            System.out.println("Not able to open file - wrong filename");
+        }
+    }
+
+     */
 }
