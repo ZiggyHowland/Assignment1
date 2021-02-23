1434b9374f2bc1d5150427ec9ad1b879c6e75cf2
Dante-Ali <77851236+Dante-Ali@users.noreply.github.com>
2021-02-23 12:00:59 +0100

    First commit again .

---
+++ assignment1/src/SalgsRapportering/FinalReporting.java
@@ -0,0 +1,14 @@
+package SalgsRapportering;
+
+
+import java.util.ArrayList;
+
+public class FinalReporting {
+
+    ArrayList<SalgsRapportering> allData = new ArrayList<SalgsRapportering>();
+
+
+    public FinalReporting(ArrayList<SalgsRapportering> allData) {
+        this.allData = allData;
+    }
+}
